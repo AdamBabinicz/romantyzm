cd8f6955a722e50c6e4c000138299675dfea5161
babinicz <babinicz@onet.pl>
2021-12-08 19:29:47 +0100

    dodałem scroll

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import GlobalStyle from "./globalStyles";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import ScrollToTop from "./components/ScrollToTop.jsx";
+// import { scrollToTop } from "react-scroll/modules/mixins/animate-scroll";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <GlobalStyle />
       <Navbar />
       <Switch>
